5a7925f712693f80cf04d8c20db0be32777831a3
Anirban Gupta <anirban1992@gmail.com>
2015-09-06 10:01:25 -0400

    Deleted dataset files Made some changes to dataset and fetcher

--- fetcher.py
+++ fetcher.py
@@ -12,7 +12,7 @@ def fetch_sgm(base_url):
     for i in range(0, 22):
         data_url = base_url.format(str(i).zfill(2))
         data = requests.get(data_url).content
-        filename = 'data{}'.format(i)
+        filename = 'data{}'.format(str(i).zfill(2))
         # let me know if you want to store the data as the same format 01.sgm or just 1.sgm,
         # also if there are any changes to document structure (as in extracting each body as a document etc)
 
