58d676986d8192823ce4079113a050a4dda8275d
Priyanka Sadavartia <priyanka@Priyankas-MacBook-Pro.local>
2015-09-11 00:06:47 -0400

    Creating dictionary containing 21,000 stemmed articles without stop words

--- data_parser.py
+++ data_parser.py
@@ -2,7 +2,25 @@ from bs4 import BeautifulSoup
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import re
+
+def vectorize_document(document):
+    """
+    Function to compute the tf-idf
+    :param document:
+    :return:
+    """
+    #Vectorize the document to compute tf-idf
+    document_tfidf_vectorizer = TfidfVectorizer(max_df=0.9,min_df=0.1)
+
+    #fit the vectorizer to the document content
+    tfidf_document_matrix = document_tfidf_vectorizer.fit_transform(document)
+    print tfidf_document_matrix.shape
+    idf = document_tfidf_vectorizer._tfidf.idf_
+    print dict(zip(document_tfidf_vectorizer.get_feature_names(), idf))
+
+
 
 
 def tokenize_and_stem(content):
@@ -43,10 +61,12 @@ def retrieve_stemmed_content(body_of_text):
     for lines in body_of_text:
         initial_sum += len(lines.split())
 
-    stemmed_body = []
-    for lines_body in body_of_text:
-        stemmed_body_row = tokenize_and_stem(lines_body)
-        stemmed_body.append(stemmed_body_row)
+    # stemmed_body = []
+    # for lines_body in body_of_text:
+    #     stemmed_body_row = tokenize_and_stem(lines_body)
+    #     stemmed_body.append(stemmed_body_row)
+
+    stemmed_body = tokenize_and_stem(body_of_text)
 
     stemmed_body_sum = 0
     for list_row_body in stemmed_body:
@@ -60,44 +80,100 @@ def parse_article(article):
         Parses each article for its respective sub elements such as topics, titles, body etc
 
         Keyword Arguments:
+            article_id: the new id of one reuters article
             article: contains exactly one reuters article
     """
-    article_body = article.body.string
-    article_topic = article.topics.string
-    article_title = article.title.string
-    article_date = article.date.string
+    article_body = article.body
+    article_topic = article.topics
+    article_title = article.title
+    article_date = article.date
+    article_place = article.place
 
-    initial_body_count, stemmed_body, final_body_count = retrieve_stemmed_content(article_body)
+    #article_reuters = article.reuters['NEWID']
+    #print article_reuters
 
-    initial_title_count, stemmed_title, final_title_count = retrieve_stemmed_content(article_title)
+    '''
+    This is to just get the string values for non-null relevant tags
+    '''
+
+
+    if(article_body != None):
+        initial_body_count, stemmed_body, final_body_count = retrieve_stemmed_content(article_body.text)
+     #   print article_body.text
+     #   print stemmed_body
+        vectorize_document(stemmed_body)
+
+    if(article_topic != None):
+        initial_topic_count, stemmed_topic, final_topic_count = retrieve_stemmed_content(article_topic.text)
+
+    if(article_title != None):
+        initial_title_count, stemmed_title, final_title_count = retrieve_stemmed_content(article_title.text)
+
+    if(article_place != None):
+        initial_place_count, stemmed_place, final_place_count = retrieve_stemmed_content(article_place.text)
 
     print article_topic
     print article_date
 
-
-def parser_sgm_file(data):
-    """
-        Function to parse SGM file and separate the tags : Filter out the tags needed now here
-        Takes in SGM file data as parameter
-
-        Keyword Arguments:
-            data: the sgm file data
-    """
-
-    parser = BeautifulSoup(data, 'html.parser')
-
-    print len(parser.findAll('reuters'))  # this contains the no of articles in one sgm file, should be 1000
-
-    for article in parser.findAll('reuters'):
-        parse_article(article)
-        break
-
-
 def main():
-    with open('dataset/data02.sgm', 'r') as f:
-        data = f.read()
+    """
+    Read sgm files and parse each article from the individual documents
+    :return:
+    """
+    article_dict_collection = []
+    for i in range(0,21):
+        filename = 'data{}'.format(str(i).zfill(2))
+        with open('dataset/'+filename+'.sgm' , 'r') as f:
+            data = f.read()
+            parser = BeautifulSoup(data, 'html.parser')
 
-    parser_sgm_file(data)
+            '''
+            Looping over each article distinguished by reuters tag , creating a dictionary out of each article of the format :
+            {
+                'Body': [u'jaguar', u'jaguar', u'plc', u'jagrl', u'sell', u'new', u'xj', u'model', u'us', u'japanes' ],
+                'Places': [u'uk'],
+                'Title': [u'jaguar', u'see', u'strong', u'growth', u'new', u'model', u'sale'],
+                'Topics': [u'earn'],
+                 u'topics': u'YES',
+                 u'lewissplit': u'TRAIN',
+                 u'newid': u'2001',
+                 u'oldid': u'18419',
+                 'Date': [u'mar'],
+                 u'cgisplit': u'TRAINING-SET'
+            }
+
+            The content of each dictionary tag is after removing stop words and stemming the contents
+
+            '''
+
+            for article in parser.findAll('reuters'):
+                article_id = article.get('newid')
+                # print article_id
+                article_dict = dict(article.attrs)
+                if(article.body != None):
+                    article_dict['Body'] = tokenize_and_stem(article.body.text)
+                else:
+                    article_dict['Body'] = ' '
+                if(article.topics != None):
+                    article_dict['Topics'] = tokenize_and_stem(article.topics.text)
+                else:
+                    article_dict['Topics'] = ' '
+                if(article.title != None):
+                    article_dict['Title'] = tokenize_and_stem(article.title.text)
+                else:
+                    article_dict['Title'] = ' '
+                if(article.places != None):
+                    article_dict['Places'] = tokenize_and_stem(article.places.text)
+                else:
+                    article_dict['Places'] = ' '
+                if(article.date != None):
+                    article_dict['Date'] = tokenize_and_stem(article.date.text)
+                else:
+                    article_dict['Date'] = ' '
+                article.clear()
+
+                article_dict_collection.append(article_dict)
+    print len(article_dict_collection)
 
 if __name__ == "__main__":
     main()
